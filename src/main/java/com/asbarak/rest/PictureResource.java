67610e3d0caf3d99e7457e2728c7ffb77d87d07c
Clairiot Erik <erik@clairiot.com>
2010-05-10 18:50:39 +0200

    change package

--- src/main/java/com/asbarak/rest/PictureResource.java
+++ src/main/java/com/asbarak/rest/PictureResource.java
@@ -1,28 +1,33 @@
 package com.asbarak.rest;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.asbarak.domain.Picture;
-import com.asbarak.exception.ResourceAccessException;
 import com.asbarak.module.PictureContentProvider;
 import com.asbarak.module.PicturePublisher;
-import com.sun.jersey.impl.ResponseBuilderImpl;
 import com.sun.jersey.spi.inject.Inject;
 
-@Path("/picture/{id}")
+@Path("/picture")
 @Component("pictureRestResource")
 @Scope("singleton")
 public class PictureResource {
@@ -37,13 +42,49 @@ public class PictureResource {
 	
 	@GET
 	@Produces("image/jpg")
+	@Path("/{id}")
 	public Response doGet(@PathParam("id") String id) {
 		try {
-			return Response.temporaryRedirect(new URI("/displayPicture?id=" + id)).build();
+			return Response.temporaryRedirect(new URI("./../displayPicture?id=" + id)).build();
 		} catch (URISyntaxException e) {
 			return Response.serverError().build();
 		}
 	}
+	
+	@POST
+	@Produces("text/plain")
+	public String uploadPicture(@Context HttpServletRequest request) {
+		log.debug("erf");
+		if (! ServletFileUpload.isMultipartContent(request))
+			return "no multi";
+		
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		ServletFileUpload uploader = new ServletFileUpload(factory);
+		
+		Picture picture = null;
+		try {
+			List<FileItem> items = uploader.parseRequest(request);
+			
+			for (FileItem item : items) {
+				if (item.isFormField()) continue;
+				
+				log.debug("item = " + item.getName());
+				try {
+					item.write(new File("/home/erik/essai.jpg"));
+					picture = picturePublisher.insertPicture("/home/erik/essai.jpg");
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+		} catch (FileUploadException e) {
+			e.printStackTrace();
+		}
+		
+		return picture != null ? "" + picture.getId() : "no upload!";
+	}
+	
 	
 	// injection
 
@@ -55,8 +96,6 @@ public class PictureResource {
 	public void setPicturePublisher(PicturePublisher picturePublisher) {
 		this.picturePublisher = picturePublisher;
 	}
-	
-	
 	
 	
 }
