aa92e1dfaef7e8ff59475c8dc05d8660ca04a4b8
Clairiot Erik <erik@clairiot.com>
2010-05-19 22:40:30 +0200

    + spring version update + openId4Java integration (facing proxy issues, with NTLM authentification...)

---
+++ src/main/java/com/clairiot/utils/ExecutionContextHolder.java
@@ -0,0 +1,117 @@
+package com.clairiot.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.openid4java.util.HttpClientFactory;
+import org.openid4java.util.ProxyProperties;
+
+public class ExecutionContextHolder {
+	
+	private String configurationPath = "configuration.properties"; // default path
+	private final Properties configuration = new Properties();
+	private static final Logger LOG = Logger.getLogger(ExecutionContextHolder.class);
+	private static final boolean DEBUG = LOG.isDebugEnabled();
+	
+	
+	
+	{
+		InputStream stream = ExecutionContextHolder.class.getResourceAsStream(configurationPath);
+		
+		if (stream != null) {
+			try {
+				configuration.load(stream);
+				LOG.info("Load configuration from " + configurationPath + " file");
+			} catch (IOException e) {
+				LOG.warn("Cannot read configuration from " + configurationPath + " file");
+			}
+		} else {
+			LOG.warn("Configuration file " + configurationPath + " does not exist");
+		}
+	}
+	
+	public void configureProxy() {
+		LOG.info("Proxy configuration for system");
+		
+		setSystemProperty("http.proxyHost");
+		setSystemProperty("http.proxyPort");
+		setSystemProperty("http.proxyUser");
+		setSystemProperty("http.proxyPassword", true);
+		
+		
+		
+		LOG.info("Proxy configuration for OpenId4Java");
+		
+		ProxyProperties proxyProps = new ProxyProperties();
+		proxyProps.setProxyHostName(getProperty("http.proxyHost"));
+		proxyProps.setProxyPort(Integer.parseInt(getProperty("http.proxyPort")));
+		proxyProps.setUserName(getProperty("http.proxyUser"));
+		proxyProps.setPassword(getProperty("http.proxyPassword"));
+		
+		
+		HttpClientFactory.setProxyProperties(proxyProps);
+	}
+	
+	/**
+	 * Seems to not work.
+	 */
+	public void proxyAuthentication() {
+		Authenticator customAuth = new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				if (DEBUG) LOG.debug("Authenticator : ask for password authentication");
+				
+				return new PasswordAuthentication(
+					getProperty("http.proxyUser"),
+					getProperty("http.proxyPassword").toCharArray()
+				);
+			}
+			
+		};
+		
+		// register this authenticator on security manager
+		LOG.info("Network connection authenticator registered");
+		Authenticator.setDefault(customAuth);
+	}
+
+	private void setSystemProperty(String key) {
+		setSystemProperty(key, false);
+	}
+	
+	private String getProperty(String key) {
+		return configuration.getProperty(key);
+	}
+
+	private void setSystemProperty(String key, boolean cryptValue) {
+		final String value = getProperty(key);
+		
+		if (value != null) {
+			System.setProperty(key, value);
+			
+			LOG.info("Set system property " + key + " to " + 
+				(cryptValue ? value.replaceAll(".", "*") : value));
+			
+		} else {
+			LOG.warn("Cannot set system property " + key + " : not found in configuration file");
+		}
+	}
+
+	public String getConfigurationPath() {
+		return configurationPath;
+	}
+
+	public void setConfigurationPath(String configurationPath) {
+		this.configurationPath = configurationPath;
+	}
+
+	// injection
+	
+
+	
+
+}
---
+++ src/test/java/com/clairiot/module/OpenIdAuthTest.java
@@ -0,0 +1,77 @@
+package com.clairiot.module;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.params.HttpClientParams;
+import org.apache.commons.httpclient.params.HttpParamsFactory;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openid4java.consumer.ConsumerException;
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.discovery.DiscoveryException;
+import org.openid4java.util.OpenID4JavaUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.clairiot.utils.ExecutionContextHolder;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext-test.xml"})
+public class OpenIdAuthTest {
+	
+	private ConsumerManager manager;
+	private String userSupplied = "ipingu.myopenid.com";
+
+	@Autowired
+	private ExecutionContextHolder context;
+
+	
+	@Before
+	public void setUp() throws ConsumerException {
+		context.configureProxy();
+		context.proxyAuthentication();
+		this.manager = new ConsumerManager();
+	}
+
+	@Test
+	public void checkConnectionByPingingGoogle___thisTestSucks___asThisMethodName() throws MalformedURLException {
+		URL google = new URL("http", "www.google.com", "/");
+		
+		try {
+			final InputStream input = google.openStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+			
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+			
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void discover() {
+		try {
+			manager.discover(userSupplied);
+		} catch (DiscoveryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
